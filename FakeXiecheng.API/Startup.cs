e50201170ee546e733d4dbd01117d8cae74f0ba5
JerryChuang <jerrychuang@nine-yi.com>
2020-10-15 20:47:10 +0800

    chap 2-6 lab:add new controller

--- FakeXiecheng.API/Startup.cs
+++ FakeXiecheng.API/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -27,9 +26,7 @@ namespace FakeXiecheng.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/test", async context => { await context.Response.WriteAsync("Hello from test!"); });
-
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapControllers();
             });
         }
     }
