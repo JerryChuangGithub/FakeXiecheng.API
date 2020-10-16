cf91a268c5521fbfdb96e42c75b124e17798a114
JerryChuang <jerrychuang@nine-yi.com>
2020-10-16 20:07:48 +0800

    chap 3-4 lab:add new tourist route picture model

---
+++ FakeXiecheng.API/Models/TouristRoutePicture.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace FakeXiecheng.API.Models
+{
+    public class TouristRoutePicture
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public Guid TouristRouteId { get; set; }
+
+        public TouristRoute TouristRoute { get; set; }
+    }
+}
