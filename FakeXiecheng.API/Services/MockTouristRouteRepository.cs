d76b3f6af6abcc4fd08769bafb150483b2385267
JerryChuang <jerrychuang@nine-yi.com>
2020-10-16 20:44:17 +0800

    chap 3-4 lab: create MockTouristRouteRepository implement ITouristRouteRepository

---
+++ FakeXiecheng.API/Services/MockTouristRouteRepository.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FakeXiecheng.API.Models;
+
+namespace FakeXiecheng.API.Services
+{
+    public class MockTouristRouteRepository : ITouristRouteRepository
+    {
+        private readonly List<TouristRoute> _routes;
+
+        public MockTouristRouteRepository()
+        {
+            if (this._routes == null)
+            {
+                this._routes = InitializeTouristRoutes();
+            }
+        }
+
+        public IEnumerable<TouristRoute> GetTouristRoutes()
+        {
+            return this._routes;
+        }
+
+        public TouristRoute GetTouristRoute(Guid touristRouteId)
+        {
+            return this._routes.FirstOrDefault(x => x.Id == touristRouteId);
+        }
+
+        private static List<TouristRoute> InitializeTouristRoutes()
+        {
+            return new List<TouristRoute>
+            {
+                new TouristRoute
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "太魯閣",
+                    Description = "風光明媚太魯閣行",
+                    OriginalPrice = 5999,
+                    Features = "<p>享受大自然</p>",
+                    Fees = "<p>自行前往</p>",
+                    Notes = "<p>注意落石</p>"
+                },
+                new TouristRoute
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "阿里山",
+                    Description = "阿里山享受芬多精",
+                    OriginalPrice = 5999,
+                    Features = "<p>遠離塵囂</p>",
+                    Fees = "<p>自行前往</p>",
+                    Notes = "<p>注意颱風季節</p>"
+                }
+            };
+        }
+    }
+}
