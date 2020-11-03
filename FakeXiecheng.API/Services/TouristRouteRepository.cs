a7d81429e94de48763fe230f0f587e8d248d9191
JerryChuang <jerrychuang@nine-yi.com>
2020-11-04 00:11:51 +0800

    chap 3-7 lab: create TouristRouteRepository.cs

---
+++ FakeXiecheng.API/Services/TouristRouteRepository.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FakeXiecheng.API.Database;
+using FakeXiecheng.API.Models;
+
+namespace FakeXiecheng.API.Services
+{
+    public class TouristRouteRepository : ITouristRouteRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TouristRouteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<TouristRoute> GetTouristRoutes()
+        {
+            return _context.TouristRoutes;
+        }
+
+        public TouristRoute GetTouristRoute(Guid touristRouteId)
+        {
+            return _context.TouristRoutes.FirstOrDefault(x => x.Id == touristRouteId);
+        }
+    }
+}
