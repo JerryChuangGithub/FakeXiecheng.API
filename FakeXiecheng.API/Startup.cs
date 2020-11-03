48bcf5ceb592bfc050ff5938dda2029549ad6fd7
JerryChuang <jerrychuang@nine-yi.com>
2020-11-04 00:11:32 +0800

    chap 3-7 lab: create app db context

--- FakeXiecheng.API/Startup.cs
+++ FakeXiecheng.API/Startup.cs
@@ -1,6 +1,9 @@
+using FakeXiecheng.API.Database;
 using FakeXiecheng.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,12 +11,23 @@ namespace FakeXiecheng.API
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddTransient<ITouristRouteRepository, MockTouristRouteRepository>();
+            services.AddTransient<ITouristRouteRepository, TouristRouteRepository>();
+            services.AddDbContext<AppDbContext>(option =>
+            {
+                option.UseSqlServer(Configuration["DbContext:ConnectionString"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
