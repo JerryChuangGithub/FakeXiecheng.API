8cf3536274f5e5891a8b84f6ff02761d23280800
JerryChuang <jerrychuang@nine-yi.com>
2020-11-04 19:12:27 +0800

    chap 3-8 lab: models add data annotation

--- FakeXiecheng.API/Models/TouristRoutePicture.cs
+++ FakeXiecheng.API/Models/TouristRoutePicture.cs
@@ -1,13 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FakeXiecheng.API.Models
 {
     public class TouristRoutePicture
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [MaxLength(100)]
         public string Url { get; set; }
 
+        // TouristRoute Class + Key Attribute Property
+        [ForeignKey("TouristRouteId")]
         public Guid TouristRouteId { get; set; }
 
         public TouristRoute TouristRoute { get; set; }
