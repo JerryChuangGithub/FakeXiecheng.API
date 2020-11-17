06c46e57aee051501916dc74e4beaf39bb576932
JerryChuang <jerrychuang@nine-yi.com>
2020-11-17 20:34:28 +0800

    chap 3-9 Lab: ef migration add data seeding part 2

--- FakeXiecheng.API/Database/AppDbContext.cs
+++ FakeXiecheng.API/Database/AppDbContext.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using FakeXiecheng.API.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace FakeXiecheng.API.Database
 {
@@ -17,14 +21,39 @@ namespace FakeXiecheng.API.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
-            {
-                Id = Guid.NewGuid(),
-                Title = "ceshititle",
-                Description = "shoming",
-                OriginalPrice = 0,
-                CreateTime = DateTime.UtcNow
-            });
+            // modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            // {
+            //     Id = Guid.NewGuid(),
+            //     Title = "ceshititle",
+            //     Description = "shoming",
+            //     OriginalPrice = 0,
+            //     CreateTime = DateTime.UtcNow
+            // });
+
+            var touristRouteJsonData = 
+                File.ReadAllText(
+                    Path.GetDirectoryName(
+                        Assembly.GetExecutingAssembly().Location) +
+                        @"/Database/touristRoutesMockData.json");
+
+            IList<TouristRoute> touristRoutes =
+                JsonConvert
+                    .DeserializeObject<IList<TouristRoute>>(touristRouteJsonData); 
+
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePictureJsonData = 
+                File.ReadAllText(
+                    Path.GetDirectoryName(
+                        Assembly.GetExecutingAssembly().Location) +
+                        @"/Database/touristRoutePicturesMockData.json");
+
+            IList<TouristRoutePicture> touristRoutePictures =
+                JsonConvert
+                    .DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData); 
+
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);    
+
             base.OnModelCreating(modelBuilder);
         }
     }
