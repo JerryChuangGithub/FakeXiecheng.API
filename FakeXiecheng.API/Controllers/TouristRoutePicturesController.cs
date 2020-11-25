c8aa6212a28db80db08a200923b1b5b106880e21
JerryChuang <jerrychuang@nine-yi.com>
2020-11-26 00:49:30 +0800

    chap 5-10 lab: create new TouristRoutePicturesController.cs implement get picture api

---
+++ FakeXiecheng.API/Controllers/TouristRoutePicturesController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using FakeXiecheng.API.Dtos;
+using FakeXiecheng.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [Route("api/TouristRoutes/{touristRouteId}/pictures")]
+    [ApiController]
+    public class TouristRoutePicturesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        private readonly ITouristRouteRepository _touristRouteRepository;
+
+        public TouristRoutePicturesController(
+            IMapper mapper,
+            ITouristRouteRepository touristRouteRepository)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _touristRouteRepository = touristRouteRepository ?? throw new ArgumentNullException(nameof(touristRouteRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetPictureListForTouristRoute(Guid touristRouteId)
+        {
+            if (_touristRouteRepository.TouristRouteExists(touristRouteId) == false)
+            {
+                return NotFound("旅遊路線不存在");
+            }
+
+            var picturesFromRepo = _touristRouteRepository.GetPictureByTouristRouteId(touristRouteId).ToList();
+            if (picturesFromRepo.Count < 1)
+            {
+                return NotFound("照片不存在");
+            }
+            
+            return Ok(_mapper.Map<IEnumerable<TouristRoutePictureDto>>(picturesFromRepo));
+        }
+    }
+}
---
+++ FakeXiecheng.API/Profiles/TouristRoutePictureProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using FakeXiecheng.API.Dtos;
+using FakeXiecheng.API.Models;
+
+namespace FakeXiecheng.API.Profiles
+{
+    public class TouristRoutePictureProfile : Profile
+    {
+        public TouristRoutePictureProfile()
+        {
+            CreateMap<TouristRoutePicture, TouristRoutePictureDto>();
+        }
+    }
+}
