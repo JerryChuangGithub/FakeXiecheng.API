403d66e6dd6bd466c50d2d0e50a174f3236d397c
JerryChuang <jerrychuang@nine-yi.com>
2020-11-29 23:46:00 +0800

    chap 7-7 lab: TouristRouteForCreationDto.cs add data annotation

--- FakeXiecheng.API/Dtos/TouristRouteForCreationDto.cs
+++ FakeXiecheng.API/Dtos/TouristRouteForCreationDto.cs
@@ -1,16 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FakeXiecheng.API.Dtos
 {
     public class TouristRouteForCreationDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [Required]
+        [MaxLength(1500)]
         public string Description { get; set; }
 
+        [Range(typeof(decimal), "0.01", "9999999999999999.99")]
         public decimal OriginalPrice { get; set; }
 
+        [Range(0.0, 1.0)]
         public double? DiscountPrice { get; set; }
 
         public DateTime CreateTime { get; set; }
