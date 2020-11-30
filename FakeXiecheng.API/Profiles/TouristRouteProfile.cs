e544ade661457c23339681bd2b0e0e1f1bdbb5e0
JerryChuang <jerrychuang@nine-yi.com>
2020-11-30 19:38:57 +0800

    chap 8-3 lab: add profile of TouristRouteForUpdateDto.cs to TouristRoute.cs

--- FakeXiecheng.API/Profiles/TouristRouteProfile.cs
+++ FakeXiecheng.API/Profiles/TouristRouteProfile.cs
@@ -24,6 +24,8 @@ namespace FakeXiecheng.API.Profiles
                 .ForMember(
                     dest => dest.Id,
                     opt => opt.MapFrom(src => Guid.NewGuid()));
+
+            CreateMap<TouristRouteForUpdateDto, TouristRoute>();
         }
     }
 }
