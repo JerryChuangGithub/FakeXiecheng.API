cf3ab4d53b03f9e3bb4aa81a005e2d13b01c42dd
JerryChuang <jerrychuang@nine-yi.com>
2020-12-03 18:44:44 +0800

    chap 11-7 lab: use entity framework core tool migration db

--- FakeXiecheng.API/Migrations/20201203103727_IdentityMigrations.Designer.cs
+++ FakeXiecheng.API/Migrations/20201203103727_IdentityMigrations.Designer.cs
@@ -4,14 +4,16 @@ using FakeXiecheng.API.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FakeXiecheng.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201203103727_IdentityMigrations")]
+    partial class IdentityMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
