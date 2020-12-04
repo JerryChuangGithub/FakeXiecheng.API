12aab31855e7295637e532a5ad331f1b49211ae3
JerryChuang <jerrychuang@nine-yi.com>
2020-12-04 08:43:53 +0800

    chap 11-10 lab: adjust AppDbContext.cs and use entity framework core tool update db

--- FakeXiecheng.API/Database/AppDbContext.cs
+++ FakeXiecheng.API/Database/AppDbContext.cs
@@ -54,7 +54,43 @@ namespace FakeXiecheng.API.Database
                 JsonConvert
                     .DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData); 
 
-            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);    
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+
+            modelBuilder.Entity<ApplicationUser>(u =>
+                u.HasMany(x => x.UserRoles)
+                    .WithOne().HasForeignKey(ur => ur.UserId).IsRequired());
+
+            const string adminRoleId = "13C30197-CDF5-4541-BBFE-3D29BFAD1DDF";
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = adminRoleId,
+                    Name = "Admin",
+                    NormalizedName = "Admin".ToUpper()
+                });
+
+            const string adminUserId = "DE8AE532-9BAA-49E0-A41C-585812AD3A5B";
+            var adminUser = new ApplicationUser
+            {
+                Id = adminUserId,
+                UserName = "admin@gmail.com",
+                NormalizedUserName = "admin@gmail.com".ToUpper(),
+                Email = "admin@gmail.com",
+                NormalizedEmail = "admin@gmail.com".ToUpper(),
+                TwoFactorEnabled = false,
+                EmailConfirmed = true,
+                PhoneNumber = "0912345678",
+                PhoneNumberConfirmed = false
+            };
+            adminUser.PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(adminUser, "Fake123$");
+            modelBuilder.Entity<ApplicationUser>().HasData(adminUser);
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = adminUserId
+                });
 
             base.OnModelCreating(modelBuilder);
         }
---
+++ FakeXiecheng.API/Migrations/20201204003907_ApplicationUserMigration.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FakeXiecheng.API.Migrations
+{
+    public partial class ApplicationUserMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "AspNetUserTokens",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "AspNetUserRoles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "AspNetUserLogins",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "AspNetUserClaims",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "13C30197-CDF5-4541-BBFE-3D29BFAD1DDF", "ca873957-4c6a-4e01-9bf5-eec311fb51fb", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "Address", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "DE8AE532-9BAA-49E0-A41C-585812AD3A5B", 0, null, "3c826781-0c87-4466-ad03-e6e6c19160fa", "admin@gmail.com", true, false, null, "ADMIN@GMAIL.COM", "ADMIN@GMAIL.COM", "AQAAAAEAACcQAAAAEE4sHV7etr4XHYZ2TNSiSxfHZqmUxUHaVpKh2d50jlMhKJO7i3SoOZK98Kv4nOj9aQ==", "0912345678", false, "4ee728e3-5acb-4bab-8144-3582b2e4bb21", false, "admin@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "UserId", "RoleId", "ApplicationUserId" },
+                values: new object[] { "DE8AE532-9BAA-49E0-A41C-585812AD3A5B", "13C30197-CDF5-4541-BBFE-3D29BFAD1DDF", null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserTokens_ApplicationUserId",
+                table: "AspNetUserTokens",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_ApplicationUserId",
+                table: "AspNetUserRoles",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_ApplicationUserId",
+                table: "AspNetUserLogins",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_ApplicationUserId",
+                table: "AspNetUserClaims",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserClaims",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserLogins",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserRoles",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserTokens",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_ApplicationUserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserTokens_ApplicationUserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_ApplicationUserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserLogins_ApplicationUserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserClaims_ApplicationUserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "UserId", "RoleId" },
+                keyValues: new object[] { "DE8AE532-9BAA-49E0-A41C-585812AD3A5B", "13C30197-CDF5-4541-BBFE-3D29BFAD1DDF" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "13C30197-CDF5-4541-BBFE-3D29BFAD1DDF");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "DE8AE532-9BAA-49E0-A41C-585812AD3A5B");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "AspNetUserClaims");
+        }
+    }
+}
