78bf3a2de936513e86e6b8c6c8e398ddbe2d4eca
JerryChuang <jerrychuang@nine-yi.com>
2020-12-09 00:19:27 +0800

    chap 13-6 lab: add OrderDto.cs and OrderProfile.cs

---
+++ FakeXiecheng.API/Dtos/OrderDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using FakeXiecheng.API.Models;
+
+namespace FakeXiecheng.API.Dtos
+{
+    public class OrderDto
+    {
+        
+        public Guid Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public ICollection<LineItemDto> OrderItems { get; set; }
+
+        public string State { get; set; }
+
+        public DateTime CreateDateUTC { get; set; }
+
+        public string TransactionMetadata { get; set; }
+    }
+}
