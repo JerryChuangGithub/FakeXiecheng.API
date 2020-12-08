a86b126a6b4c852c59ab92d62f1642add0657078
JerryChuang <jerrychuang@nine-yi.com>
2020-12-08 20:36:16 +0800

    chap 13-5 lab: use stateless package implement state machine

--- FakeXiecheng.API/Models/Order.cs
+++ FakeXiecheng.API/Models/Order.cs
@@ -1,11 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Stateless;
 
 namespace FakeXiecheng.API.Models
 {
     public class Order
     {
+        private StateMachine<OrderStateEnum, OrderStateTriggerEnum> _machine;
+
+        public Order()
+        {
+            StateMachineInit();
+        }
+
         [Key]
         public Guid Id { get; set; }
 
@@ -20,6 +28,25 @@ namespace FakeXiecheng.API.Models
         public DateTime CreateDateUTC { get; set; }
 
         public string TransactionMetadata { get; set; }
+
+        private void StateMachineInit()
+        {
+            _machine = new StateMachine<OrderStateEnum, OrderStateTriggerEnum>(OrderStateEnum.Pending);
+
+            _machine.Configure(OrderStateEnum.Pending)
+                .Permit(OrderStateTriggerEnum.PlaceOrder, OrderStateEnum.Processing)
+                .Permit(OrderStateTriggerEnum.Cancel, OrderStateEnum.Cancelled);
+
+            _machine.Configure(OrderStateEnum.Processing)
+                .Permit(OrderStateTriggerEnum.Approve, OrderStateEnum.Completed)
+                .Permit(OrderStateTriggerEnum.Reject, OrderStateEnum.Declined);
+
+            _machine.Configure(OrderStateEnum.Declined)
+                .Permit(OrderStateTriggerEnum.PlaceOrder, OrderStateEnum.Processing);
+
+            _machine.Configure(OrderStateEnum.Completed)
+                .Permit(OrderStateTriggerEnum.Return, OrderStateEnum.Refund);
+        }
     }
 
     public enum OrderStateEnum
@@ -30,5 +57,14 @@ namespace FakeXiecheng.API.Models
         Declined,
         Cancelled,
         Refund
+    }
+
+    public enum OrderStateTriggerEnum
+    {
+        PlaceOrder,
+        Approve,
+        Reject,
+        Cancel,
+        Return
     }
 }
