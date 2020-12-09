7ff7c10afe0573dd59d0e855be9046d6785ad363
JerryChuang <jerrychuang@nine-yi.com>
2020-12-09 08:36:31 +0800

    chap 13-7 lab: create OrdersController.cs and create GetOrders api

---
+++ FakeXiecheng.API/Controllers/OrdersController.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using FakeXiecheng.API.Dtos;
+using FakeXiecheng.API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [ApiController]
+    [Route("api/Orders")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        private readonly ITouristRouteRepository _touristRepository;
+
+        private readonly IMapper _mapper;
+
+        public OrdersController(
+            IHttpContextAccessor httpContextAccessor,
+            ITouristRouteRepository touristRouteRepository,
+            IMapper mapper)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _touristRepository = touristRouteRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        public async Task<IActionResult> GetOrders()
+        {
+            var userId = _httpContextAccessor
+                .HttpContext
+                .User
+                .FindFirst(ClaimTypes.NameIdentifier)
+                .Value;
+
+            var orders = await _touristRepository.GetOrdersByUserId(userId);
+
+            return Ok(_mapper.Map<IEnumerable<OrderDto>>(orders));
+        }
+    }
+}
