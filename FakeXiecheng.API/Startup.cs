721d104ba361615280117485db7c2dddd001ff77
JerryChuang <jerrychuang@nine-yi.com>
2020-12-10 00:20:42 +0800

    chap 13-8 lab: register http client in Startup.cs

--- FakeXiecheng.API/Startup.cs
+++ FakeXiecheng.API/Startup.cs
@@ -89,6 +89,8 @@ namespace FakeXiecheng.API
             });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
