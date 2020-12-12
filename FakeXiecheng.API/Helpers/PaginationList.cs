9ab7550f23b9ec3b6481ecdb8c4b59b2955fc815
JerryChuang <jerrychuang@nine-yi.com>
2020-12-12 23:36:52 +0800

    chap 14-7 lab: PaginationList.cs add new property

--- FakeXiecheng.API/Helpers/PaginationList.cs
+++ FakeXiecheng.API/Helpers/PaginationList.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace FakeXiecheng.API.Helpers
     public class PaginationList<T> : List<T>
     {
         public PaginationList(
+            int totalCount,
             int currentPage,
             int pageSie,
             IEnumerable<T> items)
@@ -15,7 +17,17 @@ namespace FakeXiecheng.API.Helpers
             CurrentPage = currentPage;
             PageSize = pageSie;
             AddRange(items);
+            TotalCount = totalCount;
+            TotalPage = (int) Math.Ceiling(totalCount / (double) pageSie);
         }
+
+        public int TotalPage { get; }
+
+        public int TotalCount { get; }
+
+        public bool HasPrevious => CurrentPage > 1;
+
+        public bool HasNext => CurrentPage < TotalPage;
 
         public int CurrentPage { get; set; }
 
@@ -23,12 +35,14 @@ namespace FakeXiecheng.API.Helpers
 
         public static async Task<PaginationList<T>> Create(int currentPage, int pageSize, IQueryable<T> query)
         {
+            var totalCount = await query.CountAsync();
+            
             var skip = (currentPage - 1) * pageSize;
             query = query.Skip(skip);
             query = query.Take(pageSize);
 
             var items = await query.ToArrayAsync();
-            return new PaginationList<T>(currentPage, pageSize, items);
+            return new PaginationList<T>(totalCount, currentPage, pageSize, items);
         }
     }
 }
